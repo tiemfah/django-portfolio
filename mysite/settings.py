2e7974a4e7f62898791b909a585943e90ec04df2
tiemfah <sivanat.sub@gmail.com>
2021-01-20 21:43:13 +0700

    set more variable for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '^+x86$e*)6+*&7&5$(knhl7x7al8zp(5a+l1^_28$!^q-of+!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
